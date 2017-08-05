226fd089b6f72c362db13643415dfa502506ba69
jfbs <phrewn@gmail.com>
2017-08-05 16:31:04 -0400

    remove Dicewds8k references

--- src/diceware/RollDice.java
+++ src/diceware/RollDice.java
@@ -3,13 +3,13 @@ package diceware;
 public class RollDice {
     public static void main(String[] args) {
         String str = "";
-        Dicewds8k ptr = new Dicewds8k();
+
         for(int i = 0; i < 5; i++) {
             Dice throwDie = new Dice();
             str += throwDie.roll();
         }
 
         System.out.println(str);
-        System.out.println(ptr.getCount());
+        
     }
 }
