d9306b980ed86bcd01bb76149c9f1d4bf9b6a540
jfbs <phrewn@gmail.com>
2017-08-05 00:53:15 -0400

    Dice class

---
+++ src/diceware/Dice.java
@@ -0,0 +1,20 @@
+package diceware;
+
+/*
+Return a random number
+from a six-sided die
+ */
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Dice {
+    private int side;
+
+    public Dice() {
+        side = ThreadLocalRandom.current().nextInt(1,7);
+    }
+
+    public int roll() {
+        return this.side;
+    }
+}
