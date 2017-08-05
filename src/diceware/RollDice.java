a0b4ab509c094aa72eb5125d922fcee9c5b926c0
jfbs <phrewn@gmail.com>
2017-08-05 15:40:38 -0400

    adapt wordlist for java

--- src/diceware/RollDice.java
+++ src/diceware/RollDice.java
@@ -2,14 +2,14 @@ package diceware;
 
 public class RollDice {
     public static void main(String[] args) {
-        //int[] hand = new int[5];
         String str = "";
+        Dicewds8k ptr = new Dicewds8k();
         for(int i = 0; i < 5; i++) {
             Dice throwDie = new Dice();
-            //hand[i] = throwDie.roll();
             str += throwDie.roll();
         }
-        
+
         System.out.println(str);
+        System.out.println(ptr.getCount());
     }
 }
