668d41158a503aa756838283412aa6e299d20590
jfbs <phrewn@gmail.com>
2017-08-05 22:19:36 -0400

    print five word passphrase

--- src/diceware/RollDice.java
+++ src/diceware/RollDice.java
@@ -4,19 +4,28 @@ public class RollDice {
     public static void main(String[] args) {
         String hand;
         String match;
+        String trimmed;
+        String passphrase = " ";
+        int hands = 5;
 
-        StringBuilder handBuilder = new StringBuilder();
-        for(int i = 0; i < 5; i++) {
-            Dice throwDie = new Dice();
-            handBuilder.append(throwDie.roll());
+        for(int j = 0; j < hands; j++) {
+            StringBuilder handBuilder = new StringBuilder();
+            for (int i = 0; i < 5; i++) {
+                Dice throwDie = new Dice();
+                handBuilder.append(throwDie.roll());
+            }
+            hand = handBuilder.toString();
+
+            //System.out.println(hand);
+
+            ScanFile sf = new ScanFile();
+            match = sf.getWord(hand);
+            trimmed = match.replace(hand, "").trim();
+            //System.out.println(trimmed);
+            passphrase += trimmed + " ";
         }
-        hand = handBuilder.toString();
 
-        //System.out.println(hand);
-
-        ScanFile sf = new ScanFile();
-        match = sf.getWord(hand);
-        System.out.println(match.replace(hand, "").trim());
+        System.out.println(passphrase.trim());
 
     }
 }
