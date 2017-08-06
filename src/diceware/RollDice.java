989784fc3376d9eaa81c2c11359a6a273c338371
jfbs <phrewn@gmail.com>
2017-08-06 16:24:28 -0400

    1000x avg. runtime: 0.044s

--- src/diceware/RollDice.java
+++ src/diceware/RollDice.java
@@ -5,31 +5,64 @@ import java.text.NumberFormat;
 
 public class RollDice {
     public static void main(String[] args) {
-        final long startTime = System.currentTimeMillis();
-        String hand, match, trimmed, passphrase;
-        int hands = 8; // passphrase length
-        StringBuilder passBuilder = new StringBuilder();
+        final int WARMUP_ITERATIONS = 100;
+        final int RUN_ITERATIONS = 1000;
 
-        for(int j = 0; j < hands; j++) {
-            StringBuilder handBuilder = new StringBuilder();
+        // warm-up
+        for (int k = 0; k < WARMUP_ITERATIONS; ++k) {
+            String hand, match, trimmed, passphrase;
+            int hands = 8; // passphrase length
+            StringBuilder passBuilder = new StringBuilder();
 
-            for (int i = 0; i < 5; i++) {  // get five dice
-                Dice throwDie = new Dice();
-                handBuilder.append(throwDie.roll());
+            for (int j = 0; j < hands; j++) {
+                StringBuilder handBuilder = new StringBuilder();
+
+                for (int i = 0; i < 5; i++) {  // get five dice
+                    Dice throwDie = new Dice();
+                    handBuilder.append(throwDie.roll());
+                }
+                hand = handBuilder.toString(); // all five dice
+
+                ScanFile sf = new ScanFile();
+                match = sf.getWord(hand);
+                trimmed = match.replace(hand, "").trim();
+                passBuilder.append(trimmed).append(" ");
             }
-            hand = handBuilder.toString(); // all five dice
+            passphrase = passBuilder.toString();
 
-            ScanFile sf = new ScanFile();
-            match = sf.getWord(hand);
-            trimmed = match.replace(hand, "").trim();
-            passBuilder.append(trimmed).append(" ");
+            //System.out.println(passphrase.trim());
         }
-        passphrase = passBuilder.toString();
 
-        System.out.println(passphrase.trim());
+        // timing
+        final long startTime = System.currentTimeMillis();
+        for (int k = 0; k < RUN_ITERATIONS; ++k) {
+            String hand, match, trimmed, passphrase;
+            int hands = 8; // passphrase length
+            StringBuilder passBuilder = new StringBuilder();
+
+            for (int j = 0; j < hands; j++) {
+                StringBuilder handBuilder = new StringBuilder();
+
+                for (int i = 0; i < 5; i++) {  // get five dice
+                    Dice throwDie = new Dice();
+                    handBuilder.append(throwDie.roll());
+                }
+                hand = handBuilder.toString(); // all five dice
+
+                ScanFile sf = new ScanFile();
+                match = sf.getWord(hand);
+                trimmed = match.replace(hand, "").trim();
+                passBuilder.append(trimmed).append(" ");
+            }
+            passphrase = passBuilder.toString();
+
+            //System.out.println(passphrase.trim());
+        }
 
         final long duration = System.currentTimeMillis() - startTime;
+        final long average = duration / RUN_ITERATIONS;
         NumberFormat fmt = new DecimalFormat("#0.000");
-        System.out.print("Runtime: " + fmt.format((duration)/1000d) + "s");
+        System.out.print("Average runtime: " + fmt.format((average)/1000d) + "s");
+        //System.out.println("average:");
     }
 }
