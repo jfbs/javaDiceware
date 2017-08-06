21750c4a3b6ef56ec2c7e6c19aae5fd5b326f7b6
jfbs <phrewn@gmail.com>
2017-08-05 22:38:44 -0400

    housekeeping

--- src/diceware/RollDice.java
+++ src/diceware/RollDice.java
@@ -2,28 +2,23 @@ package diceware;
 
 public class RollDice {
     public static void main(String[] args) {
-        String hand;
-        String match;
-        String trimmed;
-        String passphrase;
+        String hand, match, trimmed, passphrase;
         int hands = 5;
         StringBuilder passBuilder = new StringBuilder();
 
         for(int j = 0; j < hands; j++) {
+
             StringBuilder handBuilder = new StringBuilder();
+
             for (int i = 0; i < 5; i++) {
                 Dice throwDie = new Dice();
                 handBuilder.append(throwDie.roll());
             }
             hand = handBuilder.toString();
 
-            //System.out.println(hand);
-
             ScanFile sf = new ScanFile();
             match = sf.getWord(hand);
             trimmed = match.replace(hand, "").trim();
-            //System.out.println(trimmed);
-
             passBuilder.append(trimmed).append(" ");
         }
         passphrase = passBuilder.toString();
