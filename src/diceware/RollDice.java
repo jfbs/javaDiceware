fecaef1fd478345d766ad1752593684144952e26
jfbs <phrewn@gmail.com>
2017-08-05 20:43:05 -0400

    separate scan into its own class

--- src/diceware/RollDice.java
+++ src/diceware/RollDice.java
@@ -1,9 +1,5 @@
 package diceware;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-
 public class RollDice {
     public static void main(String[] args) {
         String str = "";
@@ -15,19 +11,8 @@ public class RollDice {
 
         System.out.println(str);
 
-        File file = new File("src/diceware/diceware.txt");
-        Scanner in = null;
-        try {
-            in = new Scanner(file);
-            while(in.hasNext()) {
-                String line=in.nextLine();
-                if(line.contains(str))
-                    System.out.println(line);
-            }
-        } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+        ScanFile sf = new ScanFile();
+        System.out.println(sf.getWord(str));
 
     }
 }
---
+++ src/diceware/ScanFile.java
@@ -0,0 +1,34 @@
+package diceware;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ScanFile {
+
+    private String needle;
+    private File file = new File("src/diceware/diceware.txt");
+
+    public ScanFile() {
+        needle = null;
+    }
+
+    public String getWord(String str) {
+
+        Scanner in;
+        try {
+            in = new Scanner(file);
+            while (in.hasNext()) {
+                String line = in.nextLine();
+                if (line.contains(str)) {
+                    needle = line;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return needle;
+    }
+}
